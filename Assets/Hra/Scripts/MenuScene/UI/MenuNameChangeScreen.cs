d1b855e1cade714f16acb7e717a0e8257438e2e1
Jakub Bartušek <jack.bartusek@gmail.com>
2024-08-04 17:13:58 +0200

    custom names fixed

--- Assets/Hra/Scripts/MenuScene/UI/MenuNameChangeScreen.cs
+++ Assets/Hra/Scripts/MenuScene/UI/MenuNameChangeScreen.cs
@@ -5,6 +5,7 @@ public class MenuNameChangeScreen : GameScreen
 {
     [SerializeField] private TMP_InputField _playerOneName;
     [SerializeField] private TMP_InputField _playerTwoName;
+    private const int MaxNameLength = 8;
 
     private void Start()
     {
@@ -22,18 +23,28 @@ public class MenuNameChangeScreen : GameScreen
 
     private void OnEnable()
     {
-        _playerOneName.onValueChanged.AddListener(ChangePlayerOneName);
-        _playerTwoName.onValueChanged.AddListener(ChangePlayerTwoName);
+        _playerOneName.onValueChanged.AddListener(HandlePlayerOneNameChange);
+        _playerOneName.onEndEdit.AddListener(EnforcePlayerOneNameLength);
+        _playerTwoName.onValueChanged.AddListener(HandlePlayerTwoNameChange);
+        _playerTwoName.onEndEdit.AddListener(EnforcePlayerTwoNameLength);
     }
 
     private void OnDisable()
     {
-        _playerOneName.onValueChanged.RemoveListener(ChangePlayerOneName);
-        _playerTwoName.onValueChanged.RemoveListener(ChangePlayerTwoName);
+        _playerOneName.onValueChanged.RemoveListener(HandlePlayerOneNameChange);
+        _playerOneName.onEndEdit.RemoveListener(EnforcePlayerOneNameLength);
+        _playerTwoName.onValueChanged.RemoveListener(HandlePlayerTwoNameChange);
+        _playerTwoName.onEndEdit.RemoveListener(EnforcePlayerTwoNameLength);
     }
 
-    private void ChangePlayerOneName(string newName)
+    private void HandlePlayerOneNameChange(string newName)
     {
+        if (newName.Length > MaxNameLength)
+        {
+            _playerOneName.text = newName.Substring(0, MaxNameLength);
+            newName = _playerOneName.text;
+        }
+
         _playerTwoName.interactable = newName.Length > 0;
 
         if (GameManager.Instance.PlayerNames.Count == 0)
@@ -46,8 +57,23 @@ public class MenuNameChangeScreen : GameScreen
         }
     }
 
-    private void ChangePlayerTwoName(string newName)
+    private void EnforcePlayerOneNameLength(string newName)
     {
+        if (newName.Length > MaxNameLength)
+        {
+            _playerOneName.text = newName.Substring(0, MaxNameLength);
+            GameManager.Instance.PlayerNames[0] = _playerOneName.text;
+        }
+    }
+
+    private void HandlePlayerTwoNameChange(string newName)
+    {
+        if (newName.Length > MaxNameLength)
+        {
+            _playerTwoName.text = newName.Substring(0, MaxNameLength);
+            newName = _playerTwoName.text;
+        }
+
         if (GameManager.Instance.PlayerNames.Count == 1)
         {
             GameManager.Instance.PlayerNames.Add(newName);
@@ -58,10 +84,19 @@ public class MenuNameChangeScreen : GameScreen
         }
     }
 
+    private void EnforcePlayerTwoNameLength(string newName)
+    {
+        if (newName.Length > MaxNameLength)
+        {
+            _playerTwoName.text = newName.Substring(0, MaxNameLength);
+            GameManager.Instance.PlayerNames[1] = _playerTwoName.text;
+        }
+    }
+
     public void StartGame()
     {
-        if (GameManager.Instance.PlayerNames.Count == 2 && 
-            GameManager.Instance.PlayerNames[0].Length > 0 && 
+        if (GameManager.Instance.PlayerNames.Count == 2 &&
+            GameManager.Instance.PlayerNames[0].Length > 0 &&
             GameManager.Instance.PlayerNames[1].Length > 0)
         {
             SceneLoadManager.Instance.GoMenuToGame();
